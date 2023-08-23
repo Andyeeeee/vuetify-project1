b6615c3a6b42448d21865118d57e621efb699ac6
andyeeee <10514f074@gmail.com>
2023-08-23 16:46:13 +0800

    zx

--- src/views/front/AboutUss.vue
+++ src/views/front/AboutUss.vue
@@ -78,7 +78,7 @@
     <img src="@/image/icon.jpg" alt="" >
     <div>114台北市內湖區民權東路六段296巷36號</div>
     <div>訂位專線：(02) 2631 2626</div>
-    <div>統編： 31864992</div>
+    <div>統編： 318649922</div>
   </div>
 			</section>
 
