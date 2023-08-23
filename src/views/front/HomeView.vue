6df198434b30bd613e52f2d81468bfea6ce93bd5
andyeeee <10514f074@gmail.com>
2023-08-23 16:36:45 +0800

    12

--- src/views/front/HomeView.vue
+++ src/views/front/HomeView.vue
@@ -382,13 +382,13 @@
 			</v-card>
 		</section>
 
-		<!-- <section id="footer" style="text-align: center; background-color: #003158" class="section3">
+		<section id="footer" style="text-align: center; background-color: #003158" class="section3">
 			<img src="@/image/icon.jpg" alt="" width="120" style="margin: 50px" />
 			<div>114台北市內湖區民權東路六段296巷36號</div>
 			<div>訂位專線：(02) 2631 2626</div>
 			<div>統編： 31864992</div>
-		</section> -->
-		<FooTr></FooTr>
+		</section>
+		<!-- <FooTr></FooTr> -->
 	</div>
 	<!-- <FooTer></FooTer> -->
 </template>
@@ -399,7 +399,7 @@ import { useSnackbar } from 'vuetify-use-dialog'
 import ProductCard from '@/components/ProductCard.vue'
 import layout from '@/components/FrontLayout.vue'
 import tite from '@/components/TitlePicture.vue'
-import FooTr from '@/components/FooTer.vue'
+// import FooTr from '@/components/FooTer.vue'
 import swiper from '@/components/SwiPer.vue'
 
 const createSnackbar = useSnackbar()
