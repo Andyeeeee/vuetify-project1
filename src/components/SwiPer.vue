a70d5f6210fe70e318324ace5333623b4a30a1c2
Andyeeeee <10514f074@gmail.com>
2023-10-18 10:25:48 +0800

    UpdateRWD

--- src/components/SwiPer.vue
+++ src/components/SwiPer.vue
@@ -1,45 +1,38 @@
 <template>
-  <swiper
-    :spaceBetween="20"
-    :centeredSlides="true"
-    :autoplay="{
-      delay: 4000,
-      disableOnInteraction: false,
-    }"
-    :pagination="{
-      clickable: true,
-    }"
-    :navigation="true"
-    :modules="modules"
-    class="mySwiper"
-  >
+  <swiper :spaceBetween="20" :centeredSlides="true" :autoplay="{
+    delay: 4000,
+    disableOnInteraction: false,
+  }" :pagination="{
+  clickable: true,
+}" :navigation="true" :modules="modules" class="mySwiper">
     <swiper-slide><img src="@/image/main/p1.jpg" width="100%" alt=""> </swiper-slide>
-    <swiper-slide><img src="@/image/main/p2.jpg" width="100%" alt=""></swiper-slide><swiper-slide><img src="@/image/main/p3.png" width="100%" alt=""></swiper-slide>
+    <swiper-slide><img src="@/image/main/p2.jpg" width="100%" alt=""></swiper-slide>
+    <swiper-slide><img src="@/image/main/p3.png" width="100%" alt=""></swiper-slide>
     <swiper-slide><img src="@/image/main/p4.jpg" width="100%" alt=""></swiper-slide>
   </swiper>
 </template>
 <script>
-  // Import Swiper Vue.js components
-  import { Swiper, SwiperSlide } from 'swiper/vue'
+// Import Swiper Vue.js components
+import { Swiper, SwiperSlide } from 'swiper/vue'
 
-  // Import Swiper styles
-  import 'swiper/css'
+// Import Swiper styles
+import 'swiper/css'
 
-  import 'swiper/css/pagination'
-  import 'swiper/css/navigation'
+import 'swiper/css/pagination'
+import 'swiper/css/navigation'
 
-  // import required modules
-  import { Autoplay, Pagination, Navigation } from 'swiper/modules'
+// import required modules
+import { Autoplay, Pagination, Navigation } from 'swiper/modules'
 
-  export default {
-    components: {
-      Swiper,
-      SwiperSlide,
-    },
-    setup() {
-      return {
-        modules: [Autoplay, Pagination, Navigation],
-      }
-    },
-  }
+export default {
+  components: {
+    Swiper,
+    SwiperSlide,
+  },
+  setup() {
+    return {
+      modules: [Autoplay, Pagination, Navigation],
+    }
+  },
+}
 </script>
