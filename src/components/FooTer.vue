72748883d1890b95b405243d1837e25c02bb2e83
Andyeeeee <10514f074@gmail.com>
2023-10-19 17:29:26 +0800

    11

--- src/components/FooTer.vue
+++ src/components/FooTer.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="footer">
-    <img src="@/image/icon.jpg" alt="">
+    <img src="@/image/logo.jpg" alt="">
     <div>114台北市內湖區</div>
     <div>訂位專線：(02) 1234 5678</div>
     <div>統編： xxxxxxxxx</div>
